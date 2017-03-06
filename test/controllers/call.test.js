c38301c69cc9a1caf1f137b70e5dcda5672a529e
kevintaing86 <kevin.taing@ttu.edu>
2017-03-06 17:39:08 -0600

    Started Unit testing
    -Created unit test file for call controller

---
+++ test/controllers/call.test.js
@@ -0,0 +1,13 @@
+const assert = require('assert');
+const tokenController = require('./controller/token');
+
+describe('token', function() {
+    describe('generateTwilioToken()', function() {
+        it('should return ', function() {
+
+            assert
+        });
+
+    });
+
+});

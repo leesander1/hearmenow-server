79ea18ce716794fcb07c795882d3005d6fd57fc6
kevintaing86 <kevin.taing@ttu.edu>
2017-03-07 10:58:41 -0600

    Added more test to token controller

--- test/controllers/token.test.js
+++ test/controllers/token.test.js
@@ -3,12 +3,15 @@ const app = require('../server.js');
 
 describe('token', function() {
     describe('generateTwilioToken()', () => {
-        it('should return ', (done) => {
+        it('should return send 200 response', (done) => {
             request(app)
               .get('/api/generateToken')
               .expect(200, done);
         });
 
+        it('should return a token', (done) => {
+            var blah = request(app).get('/api/generateToken');
+            console.log(blah);
+        });
     });
-
 });

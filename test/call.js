5b170a21b2d92403c92e2cdf0edbb25331f63102
kevintaing86 <kevin.taing@ttu.edu>
2017-03-27 10:56:09 -0500

    Fixed typo
    -Removed unneeded dependancy

--- test/call.js
+++ test/call.js
@@ -1,7 +1,6 @@
 const request = require('supertest');
 const app = require('../server.js');
 const chai = require('chai');
-const chaiXml = require('chai-xml');
 const should = chai.should();
 const expect = chai.expect();
 
